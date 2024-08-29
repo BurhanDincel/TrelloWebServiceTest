af10addaf599a05d41b28f6422ce5000d492a46e
BurhanDincel <dincel@gmail.com>
2024-08-29 10:37:41 +0300

    Initial commit

---
+++ src/main/java/api/TrelloApi.java
@@ -0,0 +1,90 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class TrelloApi {
+
+    private static final String BASE_URL = "https://api.trello.com/1";
+    private static final String KEY = "313cf66e2740574fe226814e7eaec39e";
+    private static final String TOKEN = "ATTA3fead77d5d0891e5e7b0f6d9b7df9a73be8fa6a322736a7c40c4306d179a79e5ED1C631A";
+
+    static {
+        RestAssured.baseURI = BASE_URL;
+    }
+
+    public Response createBoard(String boardName) {
+        return given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"" + boardName + "\"}")
+                .post("/boards")
+                .then()
+                .extract().response();
+    }
+
+    public Response createList(String listName, String idBoard) {
+        return given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"" + listName + "\", \"idBoard\": \"" + idBoard + "\"}")
+                .post("/lists")
+                .then()
+                .extract().response();
+
+    }
+
+    public Response createCard(String listId, String cardName) {
+        // JSON gövdesi oluştur
+        String jsonBody = "{\"name\": \"" + cardName + "\", \"idList\": \"" + listId + "\"}";
+
+        return RestAssured.given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .contentType(ContentType.JSON) // Content-Type'ın JSON olduğundan emin olun
+                .body(jsonBody) // JSON gövdesini gönder
+                .post("/cards")
+                .then()
+                .extract().response();
+    }
+
+    public Response updateCard(String cardId, String newName) {
+        return RestAssured.given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"" + newName + "\"}")
+                .put("/cards/" + cardId)
+                .then()
+                .extract().response();
+    }
+
+    public Response deleteCard(String cardId) {
+        return RestAssured.given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .delete("/cards/" + cardId)
+                .then()
+                .extract().response();
+    }
+
+    public Response deleteBoard(String boardId) {
+        return RestAssured.given()
+                .baseUri(BASE_URL)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .delete("/boards/" + boardId)
+                .then()
+                .extract().response();
+    }
+}
---
+++ src/test/java/tests/TrelloApiTests.java
@@ -0,0 +1,67 @@
+package tests;
+
+import api.TrelloApi;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrelloApiTests {
+
+    TrelloApi trelloApi = new TrelloApi();
+
+    @Test
+    public void trelloApiTest() {
+
+        // Board oluştur
+        Response createBoardResponse = trelloApi.createBoard("NewTestBoard");
+        String boardId = createBoardResponse.jsonPath().getString("id");
+        System.out.println("\nBoard ID: " + boardId);
+
+        String boardResponseBody = createBoardResponse.getBody().asString();
+        System.out.println("\nBoard Response Body: " + boardResponseBody);
+
+        Response createListResponse = trelloApi.createList("NewTestList", boardId);
+        String listId = createListResponse.jsonPath().getString("id");
+        System.out.println("\nlistId = " + listId);
+
+        String listResponseBody = createListResponse.getBody().asString();
+        System.out.println("\nList Response Body: " + listResponseBody);
+
+
+        // İki kart oluşturma
+        Response createCard1Response = trelloApi.createCard(listId, "NewCard1");
+        System.out.println("\ncreateCard1Response = " + createCard1Response.asString());
+        String cardId1 = createCard1Response.jsonPath().getString("id");
+        System.out.println("\ncardId1 = " + cardId1);
+
+        Response createCard2Response = trelloApi.createCard(listId, "NewCard2");
+        System.out.println("\ncreateCard1Response = " + createCard1Response.asString());
+        String cardId2 = createCard2Response.jsonPath().getString("id");
+        System.out.println("\ncardId2 = " + cardId2);
+
+        // Kartlardan birini güncelleme
+        String cardToUpdate = new Random().nextBoolean() ? cardId1 : cardId2;
+        Response updateCardResponse = trelloApi.updateCard(cardToUpdate, "Updated Card");
+        System.out.println("\nCard Successfuly updated");
+        assertEquals(200, updateCardResponse.statusCode());
+
+        // Kartları silme
+        Response deleteCard1Response = trelloApi.deleteCard(cardId1);
+        System.out.println("\nCard Successfuly deleted");
+        Response deleteCard2Response = trelloApi.deleteCard(cardId2);
+        System.out.println("\nCard Successfuly deleted");
+
+        assertEquals(200, deleteCard1Response.statusCode());
+        assertEquals(200, deleteCard2Response.statusCode());
+
+        // Board'u silme
+        Response deleteBoardResponse = trelloApi.deleteBoard(boardId);
+        System.out.println("\nBoard Successfuly deleted");
+        assertEquals(200, deleteBoardResponse.statusCode());
+    }
+}
